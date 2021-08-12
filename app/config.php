aa726baf7baad63142d33da2e10a8a1fabaedd2c
class-kouta <ff03csm26@gmail.com>
2021-08-12 20:49:50 +0900

    config.php を リポジトリから削除

--- app/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-define('DSN','mysql:host=localhost;dbname=my_recipe;charset=utf8');
-define('DB_USER','root');
-define('DB_PASS','root');
-
-require_once(__DIR__.'/Database.php');
-require_once(__DIR__.'/Utils.php');
