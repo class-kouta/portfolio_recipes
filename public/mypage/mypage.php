4c97027b458c7fab8708eac6e6a1ccb226cbb704
class-kouta <ff03csm26@gmail.com>
2021-08-19 07:27:11 +0900

    リンク先の修正

--- public/mypage/mypage.php
+++ public/mypage/mypage.php
@@ -24,8 +24,8 @@ require_once(__DIR__ . '/../../app/session.php');
 
   <main class="container-sm mx-2 mt-4 row">
     <a class="btn btn-outline-success col-3 py-5 fs-3" role="button" href="ja/recipes_list.php">和食</a>
-    <a class="btn btn-outline-primary col-3 py-5 fs-3 offset-1" href="recipes_we.php">洋食</a>
-    <a class="btn btn-outline-danger col-3 py-5 fs-3 offset-1" href="recipes_ch.php">中華</a>
+    <a class="btn btn-outline-primary col-3 py-5 fs-3 offset-1" href="we/recipes_we.php">洋食</a>
+    <a class="btn btn-outline-danger col-3 py-5 fs-3 offset-1" href="ch/recipes_ch.php">中華</a>
   </main>
     
   </body>
