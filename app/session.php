3126c4be9a78a8272903d1030597c35d52544d1e
class-kouta <ff03csm26.en@gmial.com>
2022-07-12 13:45:10 +0900

    パスの修正

--- app/session.php
+++ app/session.php
@@ -6,7 +6,7 @@ if(!isset($_SESSION['login'])){
   echo'ログインされていません。';
   echo '<br>';
   echo '<br>';
-  echo '<a href="../login/login.php">ログイン画面へ</a><br>';
+  echo '<a href="/portfolio_recipes/public/login/login.php">ログイン画面へ</a><br>';
   exit();
 
 }
